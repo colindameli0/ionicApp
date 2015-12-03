4995c80db623a21147404a8892c1c1b6cd9dc0e1
colindameli0 <colin.damelio@gmail.com>
2015-12-03 12:51:53 -0500

    removing some JS, adding some new functionality

--- todo/www/js/app.js
+++ todo/www/js/app.js
@@ -1,14 +1,9 @@
 angular.module('todo', ['ionic'])
 .controller('MainCtrl', function($scope, $ionicModal){
 
-  var defaults = [
-    {title: "Watch that Netflix series"},
-    {title: "Add your new tasks!"}
-  ];
-
   //Find a value in the 'myData' key in localStorage, then load that
   //Since its now a string and $scope.tasks is a array, we need to convert it back with the function JSON.parse() and pass in the task list as an string
-  $scope.tasks = JSON.parse(localStorage.getItem('myData')) || defaults
+  $scope.tasks = JSON.parse(localStorage.getItem('myData'))
   function updateStorage() {
     //call the JSON.stringify() function and pass in the $scope.tasks , this will convert the array into just a string
     localStorage.setItem('myData', JSON.stringify($scope.tasks));
@@ -46,7 +41,6 @@ angular.module('todo', ['ionic'])
   };
 
   //INITIATE ABILITY TO ERASE TASKS
-  $scope.isActive = false;
   $scope.eraseTasks = function() {
     $scope.isActive = !$scope.isActive;
   };
@@ -59,40 +53,9 @@ angular.module('todo', ['ionic'])
 
 })
 
-
-
 //SECONDARY 'SETTINGS' CONTROLLER
 .controller('SidebarCtrl', function($ionicModal){
   var vm = {};
   vm.set = 'SETTINGS';
   return vm;
 });
-
-
-
-
-/* 
-
-App.js 
-
-  angular.module('App', ['ionic', 'sidebar', 'todo'])
-  .controller('ApppCtrl, function($scope, $ionicModal);
-
-
-SideBar
-  - side-bar.js
-  - side-bar.css/sass
-  - side-bar.html
-
-  angular.module('sidebar', ['ionic'])
-  .controller('SidebarCtrl, function($scope, $ionicModal);
-
-Todo
-  -todo.js
-  -todo.css/sass
-  -todo.html
-
-  angular.module('todo', ['ionic'])
-  .controller('TodoCtrl', function($scope, $ionicModal);
-
-*/
